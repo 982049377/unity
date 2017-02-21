b05081865b7e1abf9dccec3e8337349444ce111d
王恒尊 <982049377@qq.com>
2017-02-21 11:09:15 +0800

    no message

---
+++ Assets/背包/Script/View/GridUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GridUI : MonoBehaviour ,IPointerEnterHandler,IPointerExitHandler{
+
+	public void OnPointerEnter(PointerEventData eventDate){
+	
+	
+	}
+	public void OnPointerExit(PointerEventData eventDate){
+
+
+	}
+}
